32eff955f3b743577c8905561673611693b7c5ac
pachi <carsan15@ucm.es>
2019-03-27 20:27:15 +0100

    Añadido unity y el plugin iniciado

---
+++ 3D Terrain Generator Plugin/Assets/Scripts/CustomTerrain.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+[ExecuteInEditMode]
+public class CustomTerrain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 3D Terrain Generator Plugin/Assets/Scripts/Editor/CustomTerrainEditor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+using EditorGUITable;
+
+[CustomEditor(typeof(CustomTerrain))]
+[CanEditMultipleObjects]
+
+public class CustomTerrainEditor : Editor
+{
+
+    void OnEnable() {
+
+    }
+
+    //Graphical User Interface on the editor
+    public override void OnInspectorGUI() {
+
+
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
