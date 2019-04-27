3935c1c40254619bda5ec9058ff21ccac808b57b
pachi <carsan15@ucm.es>
2019-04-27 20:07:44 +0200

    Unity Plugin avanzando

--- 3D Terrain Generator Plugin/Assets/Scripts/CustomTerrain.cs
+++ 3D Terrain Generator Plugin/Assets/Scripts/CustomTerrain.cs
@@ -8,6 +8,151 @@ using System.Linq;
 [ExecuteInEditMode]
 public class CustomTerrain : MonoBehaviour
 {
+
+
+    //SPLATMAPS ---------------------------------------------
+    [System.Serializable]
+    public class SplatHeights
+    {
+        public Texture2D texture = null;
+        public float minHeight = 0.1f;
+        public float maxHeight = 0.2f;
+        public float minSlope = 0;
+        public float maxSlope = 1.5f;
+        public Vector2 tileOffset = new Vector2(0, 0);
+        public Vector2 tileSize = new Vector2(50, 50);
+        public float splatOffset = 0.1f;
+        public float splatNoiseXScale = 0.01f;
+        public float splatNoiseYScale = 0.01f;
+        public float splatNoiseScaler = 0.1f;
+        public bool remove = false;
+    }
+
+    public List<SplatHeights> splatHeights = new List<SplatHeights>()
+    {
+        new SplatHeights()
+    };
+
+
+    public Terrain terrain;
+    public TerrainData terrainData;
+
+    public void AddNewSplatHeight()
+    {
+        splatHeights.Add(new SplatHeights());
+    }
+
+    public void RemoveSplatHeight()
+    {
+        List<SplatHeights> keptSplatHeights = new List<SplatHeights>();
+        for (int i = 0; i < splatHeights.Count; i++)
+        {
+            if (!splatHeights[i].remove)
+            {
+                keptSplatHeights.Add(splatHeights[i]);
+            }
+        }
+        if (keptSplatHeights.Count == 0) //don't want to keep any
+        {
+            keptSplatHeights.Add(splatHeights[0]); //add at least 1
+        }
+        splatHeights = keptSplatHeights;
+    }
+
+    float GetSteepness(float[,] heightmap, int x, int y, int width, int height)
+    {
+        float h = heightmap[x, y];
+        int nx = x + 1;
+        int ny = y + 1;
+
+        //if on the upper edge of the map find gradient by going backward.
+        if (nx > width - 1) nx = x - 1;
+        if (ny > height - 1) ny = y - 1;
+
+        float dx = heightmap[nx, y] - h;
+        float dy = heightmap[x, ny] - h;
+        Vector2 gradient = new Vector2(dx, dy);
+
+        float steep = gradient.magnitude;
+
+        return steep;
+    }
+
+    public void SplatMaps()
+    {
+        SplatPrototype[] newSplatPrototypes;
+        newSplatPrototypes = new SplatPrototype[splatHeights.Count];
+        int spindex = 0;
+        foreach (SplatHeights sh in splatHeights)
+        {
+            newSplatPrototypes[spindex] = new SplatPrototype();
+            newSplatPrototypes[spindex].texture = sh.texture;
+            newSplatPrototypes[spindex].tileOffset = sh.tileOffset;
+            newSplatPrototypes[spindex].tileSize = sh.tileSize;
+            newSplatPrototypes[spindex].texture.Apply(true);
+            spindex++;
+        }
+        terrainData.splatPrototypes = newSplatPrototypes;
+
+        float[,] heightMap = terrainData.GetHeights(0, 0, terrainData.heightmapWidth,
+                                                          terrainData.heightmapHeight);
+        float[,,] splatmapData = new float[terrainData.alphamapWidth,
+                                               terrainData.alphamapHeight,
+                                               terrainData.alphamapLayers];
+
+        for (int y = 0; y < terrainData.alphamapHeight; y++)
+        {
+            for (int x = 0; x < terrainData.alphamapWidth; x++)
+            {
+                float[] splat = new float[terrainData.alphamapLayers];
+                for (int i = 0; i < splatHeights.Count; i++)
+                {
+                    float noise = Mathf.PerlinNoise(x * splatHeights[i].splatNoiseXScale,
+                                                    y * splatHeights[i].splatNoiseYScale)
+                                       * splatHeights[i].splatNoiseScaler;
+                    float offset = splatHeights[i].splatOffset + noise;
+                    float thisHeightStart = splatHeights[i].minHeight - offset;
+                    float thisHeightStop = splatHeights[i].maxHeight + offset;
+                    //float steepness = GetSteepness(heightMap, x, y, 
+                    //                               terrainData.heightmapWidth, 
+                    //                               terrainData.heightmapHeight);
+
+                    float steepness = terrainData.GetSteepness(y / (float)terrainData.alphamapHeight,
+                                           x / (float)terrainData.alphamapWidth);
+
+                    if ((heightMap[x, y] >= thisHeightStart && heightMap[x, y] <= thisHeightStop) &&
+                        (steepness >= splatHeights[i].minSlope && steepness <= splatHeights[i].maxSlope))
+                    {
+                        splat[i] = 1;
+                    }
+                }
+                NormalizeVector(splat);
+                for (int j = 0; j < splatHeights.Count; j++)
+                {
+                    splatmapData[x, y, j] = splat[j];
+                }
+            }
+        }
+        terrainData.SetAlphamaps(0, 0, splatmapData);
+    }
+
+    void NormalizeVector(float[] v)
+    {
+        float total = 0;
+        for (int i = 0; i < v.Length; i++)
+        {
+            total += v[i];
+        }
+
+        for (int i = 0; i < v.Length; i++)
+        {
+            v[i] /= total;
+        }
+    }
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
--- 3D Terrain Generator Plugin/Assets/Scripts/Editor/CustomTerrainEditor.cs
+++ 3D Terrain Generator Plugin/Assets/Scripts/Editor/CustomTerrainEditor.cs
@@ -7,16 +7,71 @@ using EditorGUITable;
 
 public class CustomTerrainEditor : Editor
 {
+    GUITableState splatMapTable;
+    SerializedProperty splatHeights;
+
+
+    bool showSplatMaps = false;
+
 
     void OnEnable() {
 
+        splatMapTable = new GUITableState("splatMapTable");
+        splatHeights = serializedObject.FindProperty("splatHeights");
+
+
     }
 
+    Vector2 scrollPos;
     //Graphical User Interface on the editor
     public override void OnInspectorGUI() {
 
+        serializedObject.Update();
+
+        CustomTerrain terrain = (CustomTerrain)target;
 
 
+        //Scrollbar Starting Code
+        Rect r = EditorGUILayout.BeginVertical();
+        scrollPos =
+            EditorGUILayout.BeginScrollView(scrollPos, GUILayout.Width(r.width), GUILayout.Height(r.height));
+        EditorGUI.indentLevel++;
+
+
+
+        showSplatMaps = EditorGUILayout.Foldout(showSplatMaps, "Splat Maps");
+        if (showSplatMaps)
+        {
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            GUILayout.Label("Splat Maps", EditorStyles.boldLabel);
+            /*EditorGUILayout.Slider(splatOffset, 0, 0.1f, new GUIContent("Offset"));
+            EditorGUILayout.Slider(splatNoiseXScale, 0.001f, 1, new GUIContent("Noise X Scale"));
+            EditorGUILayout.Slider(splatNoiseYScale, 0.001f, 1, new GUIContent("Noise Y Scale"));
+            EditorGUILayout.Slider(splatNoiseScaler, 0, 1, new GUIContent("Noise Scaler"));*/
+            splatMapTable = GUITableLayout.DrawTable(splatMapTable,
+                                            serializedObject.FindProperty("splatHeights"));
+            GUILayout.Space(20);
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("+"))
+            {
+                terrain.AddNewSplatHeight();
+            }
+            if (GUILayout.Button("-"))
+            {
+                terrain.RemoveSplatHeight();
+            }
+            EditorGUILayout.EndHorizontal();
+            if (GUILayout.Button("Apply SplatMaps"))
+            {
+                terrain.SplatMaps();
+            }
+        }
+
+        //Scrollbar ending code
+        EditorGUILayout.EndScrollView();
+        EditorGUILayout.EndVertical();
+
+        serializedObject.ApplyModifiedProperties();
     }
 
 
